237702cf07e15d54d2bc49df015640727a0983fa
shinydev618 <shinydev618@gmail.com>
2022-08-04 17:11:32 -0400

    fixxing node server

--- src/redux/actions/production.tsx
+++ src/redux/actions/production.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function get_batch_list() {
   return axios.get("/api/production/get_batch_list").then((res) => {
-    console.log(res.data.batch_data);
+    console.log(res.data.batch_list);
     console.log(res.data)
     return res.data;
     // console.log(res.data.batch_list);
@@ -13,7 +13,7 @@ export function get_batch_data(batch_num: any) {
   return axios
     .post("/api/production/get_batch_data", { batch_num })
     .then((res) => {
-      console.log(res.data.batch_list);
+      console.log(res.data.batch_data);
 
       return res.data;
     });
